a3854ca81920b7c6c76ff0f2b5355eacae3b849e
MeArio IC0LD <meario@localhost.localdomain>
2017-07-13 19:44:41 +0300

    added the ability to load a rom into memory

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ MEM_SIZE = 4096
 WIDTH = 64
 HEIGHT = 32
 SCALE = 10
+OFFSET = 0x200
 
-aram = RAM(MEM_SIZE)
+aram = RAM(MEM_SIZE, OFFSET)
 adisplay = Display(WIDTH, HEIGHT, SCALE)
 acpu = CPU(aram, adisplay)
 
 if __name__ == '__main__':
     print("sup")
-
-pyglet.app.run()
+    aram.load_rom('breakout.ch8')
+    print(aram.memory)
+    pyglet.app.run()
--- ram.py
+++ ram.py
@@ -1,4 +1,12 @@
 class RAM:
-    def __init__(self, mem_size):
+    def __init__(self, mem_size, offset):
         self.MEM_SIZE = mem_size  # Memory size
         self.memory = [0] * mem_size  # Memory array
+        self.offset = offset
+
+    def load_rom(self, filename):
+        rom = open(filename, 'rb').read()
+        i = 0
+        while i < len(rom):
+            self.memory[i + self.offset] = hex(rom[i])
+            i += 1
